5339d47613f07ec935c12454e7a6944edcf98b9c
Jakob Körber <jakob.koerber@tum.de>
2022-12-13 11:44:59 +0100

    Initial Commit

---
+++ JourneyBuilder/JourneyBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyBuilderApp.swift
+//  JourneyBuilder
+//
+//  Created by Jakob Paul Körber on 13.12.22.
+//
+
+import SwiftUI
+
+@main
+struct JourneyBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
